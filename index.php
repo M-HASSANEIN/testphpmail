f15d319e079ef2240a676d97f86720c2c2cc4bf9
mohamed hassanein <momed999@hotmail.com>
2021-06-07 15:06:46 +0200

    phpmailer

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
+
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'models/Email.php';
+$model = new Email();
+
+
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    tiko
+
+</body>
+
+</html>
---
+++ models/Email.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+class Email
+{
+    protected $mail;
+    
+    public function __construct()
+    {
+        
+     
+        //Instantiation and passing `true` enables exceptions
+        $this->mail = new PHPMailer(true);
+            
+        try {
+            //Server settings
+            $this->mail->isSMTP();                                            //Send using SMTP
+            $this->mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $this->mail->Username   = 'betelgueser.hurgada@gmail.com';                     //SMTP username
+            $this->mail->Password   = 'Bet@2020';                               //SMTP password
+            $this->mail->SMTPSecure = "TLS";         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $this->mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        
+            //Recipients
+            $this->mail->setFrom('momed1081986@gmail.com', 'BETELGUESER');
+            $this->mail->addAddress('momed999@hotmail.com');     //Add a recipient
+            /*$this->mail->addAddress('ellen@example.com');       */         //Name is optional
+            $this->mail->addReplyTo('momed1081986@gmail.com', 'Information');
+            /* $this->mail->addCC('cc@example.com');
+            $this->mail->addBCC('bcc@example.com'); */
+        
+            //Attachments
+            /*$this->mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            $this->mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name */
+        
+            //Content
+            $this->mail->isHTML(true);                                  //Set email format to HTML
+            $this->mail->Subject = 'Here is the subject';
+            $this->mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $this->mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+        }
+       
+       
+    }
+}    
